37d1321f11eecf8b2b04939de9909121367ce449
karroumi <anasskarroumi2016@gmail.com>
2024-07-07 15:57:28 +0200

    / -----create function (show search bar )

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+// -----create function (show search bar )
 let searchBtn = document.querySelector("#search-btn");
 let searchForm = document.querySelector(".search-form");
 
 function showbar() {
   searchBtn.classList.toggle("fa-times");
-  searchForm.classList.toggle("active")
+  searchForm.classList.toggle("active");
+}
+
+// -----create function (show login-Form )
+let loginBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector(".login-form");
+let hideBtn = document.querySelector("#hide-btn");
+
+function showlogin() {
+  loginForm.classList.add("active");
+}
+function hidinglogin() {
+  loginForm.classList.remove("active");
 }
