4988159d0762c978c42cd9eddb7250e2b00d5aeb
karroumi <anasskarroumi2016@gmail.com>
2024-07-13 13:49:06 +0200

    add responsive to the sidebar & hide the sidebar when clicking anywhere outside of it

--- main.js
+++ main.js
@@ -2,11 +2,20 @@
  let menuIcon = document.getElementById("menu-icon");
  let slideBar = document.getElementById("slide-bar");
   function showMenu(){
-    //  slideBar.style.left = "0";
      menuIcon.classList.toggle("fa-times");
      slideBar.classList.toggle("active");
-     
  }
+//hide the sidebar when clicking anywhere outside of it
+ document.addEventListener('click', function(event) {
+  if (!slideBar.contains(event.target) && !menuIcon.contains(event.target)) {
+    if (slideBar.classList.contains('active')) {
+      slideBar.classList.remove('active');
+      menuIcon.classList.remove('fa-times');
+    }
+  }
+});
+
+// ******************************************************************************************
 
 // -----create function (show search bar )
 let searchBtn = document.querySelector("#search-btn");
