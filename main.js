4f7eab05a2c6996f587cffda5310214a18288a35
karroumi <anasskarroumi2016@gmail.com>
2024-07-20 14:54:25 +0200

    create function for section slider ( using foreach to add  color blue to the span when I click on it )

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //------create function slide-bar(hidding & showing)
- let menuIcon = document.getElementById("menu-icon");
- let slideBar = document.getElementById("slide-bar");
-  function showMenu(){
-     menuIcon.classList.toggle("fa-times");
-     slideBar.classList.toggle("active");
- }
+let menuIcon = document.getElementById("menu-icon");
+let slideBar = document.getElementById("slide-bar");
+function showMenu() {
+  menuIcon.classList.toggle("fa-times");
+  slideBar.classList.toggle("active");
+}
 //hide the sidebar when clicking anywhere outside of it
- document.addEventListener('click', function(event) {
+document.addEventListener("click", function (event) {
   if (!slideBar.contains(event.target) && !menuIcon.contains(event.target)) {
-    if (slideBar.classList.contains('active')) {
-      slideBar.classList.remove('active');
-      menuIcon.classList.remove('fa-times');
+    if (slideBar.classList.contains("active")) {
+      slideBar.classList.remove("active");
+      menuIcon.classList.remove("fa-times");
     }
   }
 });
@@ -27,18 +27,16 @@ function showbar() {
 }
 
 //hide the searchForm when clicking anywhere outside of it
-document.addEventListener('click', function(event) {
+document.addEventListener("click", function (event) {
   if (!searchForm.contains(event.target) && !searchBtn.contains(event.target)) {
-    if (searchForm.classList.contains('active')) {
-      searchForm.classList.remove('active');
-      searchBtn.classList.remove('fa-times');
+    if (searchForm.classList.contains("active")) {
+      searchForm.classList.remove("active");
+      searchBtn.classList.remove("fa-times");
     }
   }
 });
 
-// ******************************************************************************************
-
-
+// **************************************************************************************
 // -----create function (show login-Form )
 let loginBtn = document.querySelector("#login-btn");
 let loginForm = document.querySelector(".login-form");
@@ -47,8 +45,18 @@ let hideBtn = document.querySelector("#hide-btn");
 function showlogin() {
   loginForm.classList.add("active");
 }
-function hidinglogin(){
+function hidinglogin() {
   loginForm.classList.remove("active");
 }
 
 // ******************************************************************************************
+//-----------------------------create function for section slider
+let vidBtn = document.querySelectorAll(".video-btn");
+
+ // using foreach to remove the color of the span when I click on it
+vidBtn.forEach((slide) => {
+  slide.addEventListener("click", function () {
+    document.querySelector(".controls .blue").classList.remove("blue");
+    slide.classList.add("blue");
+  });
+});
